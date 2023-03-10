59ac2378c516d052efbbfdc6838ccb550665acae
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-03-10 23:59:20 +0900

    first import

---
+++ main.go
@@ -0,0 +1,340 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/golang/freetype/truetype"
+	"github.com/mattn/go-runewidth"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+	"github.com/nfnt/resize"
+	"golang.org/x/exp/slices"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+	_ "golang.org/x/image/webp"
+)
+
+type Profile struct {
+	Website     string `json:"website"`
+	Nip05       string `json:"nip05"`
+	Picture     string `json:"picture"`
+	Lud16       string `json:"lud16"`
+	DisplayName string `json:"display_name"`
+	About       string `json:"about"`
+	Name        string `json:"name"`
+}
+
+func upload(buf *bytes.Buffer) (string, error) {
+	req, err := http.NewRequest(http.MethodPost, "https://void.cat/upload?cli=true", buf)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("V-Content-Type", "image/png")
+	result := sha256.Sum256(buf.Bytes())
+	req.Header.Set("V-Full-Digest", hex.EncodeToString(result[:]))
+	req.Header.Set("V-Filename", "image.png")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer req.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func makeImage(name, content, picture string) (string, error) {
+	b, err := ioutil.ReadFile(filepath.Join(baseDir, "background.png"))
+	back, _, err := image.Decode(bytes.NewReader(b))
+	if err != nil {
+		return "", err
+	}
+	bounds := back.Bounds()
+
+	resp, err := http.Get(picture)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	img, _, err := image.Decode(bytes.NewReader(b))
+	if err != nil {
+		return "", err
+	}
+	img = resize.Resize(0, uint(bounds.Dy()), img, resize.Lanczos3)
+
+	b, err = ioutil.ReadFile(fontFn)
+	if err != nil {
+		return "", err
+	}
+	ft, err := truetype.Parse(b)
+	if err != nil {
+		return "", err
+	}
+	opt := truetype.Options{
+		Size:              25,
+		DPI:               0,
+		Hinting:           0,
+		GlyphCacheEntries: 0,
+		SubPixelsX:        0,
+		SubPixelsY:        0,
+	}
+	dst := image.NewRGBA(bounds)
+	draw.Draw(dst, bounds, &image.Uniform{color.Black}, image.ZP, draw.Src)
+
+	mask := image.NewAlpha(bounds)
+	for x := 0; x < bounds.Dx(); x++ {
+		for y := 0; y < bounds.Dy(); y++ {
+			_, _, _, a := back.At(x, y).RGBA()
+			mask.SetAlpha(x, y, color.Alpha{uint8(255 - a)})
+		}
+	}
+	fore := image.NewGray16(img.Bounds())
+	for x := 0; x < bounds.Dx(); x++ {
+		for y := 0; y < bounds.Dy(); y++ {
+			fore.Set(x, y, color.GrayModel.Convert(img.At(x, y)))
+		}
+	}
+	draw.DrawMask(dst, dst.Bounds(), fore, image.ZP, mask, image.ZP, draw.Over)
+
+	face := truetype.NewFace(ft, &opt)
+	dr := &font.Drawer{
+		Dst:  dst,
+		Src:  image.White,
+		Face: face,
+		Dot:  fixed.Point26_6{},
+	}
+	var i int
+	var line string
+	var buf bytes.Buffer
+	for i, line = range strings.Split(content, "\n") {
+		buf.WriteString(runewidth.Wrap(line, 40) + "\n")
+	}
+	for i, line = range strings.Split(buf.String(), "\n") {
+		dr.Dot.X = (fixed.I(480))
+		dr.Dot.Y = fixed.I(100 + i*30)
+		dr.DrawString(line)
+	}
+	dr.Dot.X = (fixed.I(480))
+	dr.Dot.Y = fixed.I(100 + (i+2)*30)
+	dr.DrawString(name)
+
+	buf.Reset()
+	err = png.Encode(&buf, dst)
+	if err != nil {
+		return "", err
+	}
+	return upload(&buf)
+}
+
+var (
+	rs = []string{
+		"wss://relay-jp.nostr.wirednet.jp",
+		"wss://relay.snort.social",
+		"wss://relay.damus.io",
+	}
+	baseDir string
+	nsec    string
+	fontFn  string
+)
+
+func init() {
+	if dir, err := os.Executable(); err != nil {
+		log.Fatal(err)
+	} else {
+		baseDir = filepath.Dir(dir)
+	}
+	nsec = os.Getenv("MAKEITQUOTE_NSEC")
+	if nsec == "" {
+		log.Fatal("MAKEITQUOTE_NSEC is not set")
+	}
+}
+
+func postEvent(rs []string, id, content string) error {
+	ev := nostr.Event{}
+
+	var sk string
+	if _, s, err := nip19.Decode(nsec); err != nil {
+		return err
+	} else {
+		sk = s.(string)
+	}
+	if pub, err := nostr.GetPublicKey(sk); err == nil {
+		if _, err := nip19.EncodePublicKey(pub); err != nil {
+			return err
+		}
+		ev.PubKey = pub
+	} else {
+		return err
+	}
+	ev.Content = content
+	ev.CreatedAt = time.Now()
+	ev.Kind = nostr.KindTextNote
+	ev.Tags = ev.Tags.AppendUnique(nostr.Tag{"e", id, "", "reply"})
+	ev.Sign(sk)
+	success := 0
+	for _, r := range rs {
+		relay, err := nostr.RelayConnect(context.Background(), r)
+		if err != nil {
+			continue
+		}
+		status := relay.Publish(context.Background(), ev)
+		relay.Close()
+		if status != nostr.PublishStatusFailed {
+			success++
+		}
+	}
+	if success == 0 {
+		return errors.New("failed to publish")
+	}
+	return nil
+}
+
+func findEvents(rs []string, filter nostr.Filter) []*nostr.Event {
+	for _, r := range rs {
+		relay, err := nostr.RelayConnect(context.Background(), r)
+		if err != nil {
+			continue
+		}
+		evs := relay.QuerySync(context.Background(), filter)
+		relay.Close()
+		if len(evs) > 0 {
+			return evs
+		}
+	}
+	return nil
+}
+
+func generate(rs []string, id string) (string, error) {
+	evs := findEvents(rs, nostr.Filter{
+		Kinds: []int{nostr.KindTextNote},
+		IDs:   []string{id},
+		Limit: 1,
+	})
+	if len(evs) == 0 {
+		return "", errors.New("cannot find quoted note")
+	}
+	content := evs[0].Content
+
+	evs = findEvents(rs, nostr.Filter{
+		Kinds:   []int{nostr.KindSetMetadata},
+		Authors: []string{evs[0].PubKey},
+	})
+	if len(evs) == 0 {
+		return "", errors.New("cannot find author")
+	}
+	var profile Profile
+	err := json.NewDecoder(strings.NewReader(evs[0].Content)).Decode(&profile)
+	if err != nil {
+		return "", err
+	}
+	name := profile.DisplayName + " (" + profile.Name + ")"
+	img, err := makeImage(name, content, profile.Picture)
+	if err != nil {
+		return "", err
+	}
+	return img + ".png", nil
+}
+
+func main() {
+	flag.StringVar(&fontFn, "f", filepath.Join(baseDir, "Koruri-Bold.ttf"), "font filename")
+	flag.Parse()
+	if flag.NArg() > 0 {
+		var id string
+		if _, tmp, err := nip19.Decode(flag.Arg(0)); err != nil {
+			log.Fatal(err)
+		} else {
+			id = tmp.(string)
+		}
+		img, err := generate(rs, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, img)
+		return
+	}
+
+	var err error
+	var ids []string
+	b, err := ioutil.ReadFile(filepath.Join(baseDir, "done.log"))
+	if err == nil {
+		ids = strings.Split(strings.TrimSpace(string(b)), "\n")
+	}
+	searchRs := []string{"wss://relay.nostr.band"}
+	evs := findEvents(searchRs, nostr.Filter{
+		Kinds:  []int{nostr.KindTextNote},
+		Search: "#makeitquote",
+		Limit:  10,
+	})
+	if len(evs) == 0 {
+		return
+	}
+	for _, ev := range evs {
+		id, err := nip19.EncodeNote(ev.ID)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if slices.Contains(ids, id) {
+			continue
+		}
+		if !strings.Contains(ev.Content, "#makeitquote") {
+			continue
+		}
+		fmt.Println(ev.ID)
+
+		evs = findEvents(rs, nostr.Filter{
+			Kinds: []int{nostr.KindTextNote},
+			IDs:   []string{ev.ID},
+			Limit: 1,
+		})
+		if len(evs) == 0 {
+			continue
+		}
+		p := evs[0].Tags.GetLast([]string{"e"})
+		if p == nil {
+			continue
+		}
+		img, err := generate(rs, p.Value())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = postEvent(rs, ev.ID, img)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if note, err := nip19.EncodeNote(ev.ID); err == nil {
+			ids = append(ids, note)
+		}
+	}
+	ioutil.WriteFile(filepath.Join(baseDir, "done.log"), []byte(strings.Join(ids, "\n")), 0644)
+}
